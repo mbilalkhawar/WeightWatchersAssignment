0585d3d8d3c1ca169c818965c9da5490bd78f812
Biaz-Mac <Biaz-Mac@muhammasmacbook.home>
2019-10-25 10:51:30 -0400

    Updated TestUtil File

--- WeightWatchers_ByBilal/src/weightwatchersassignment/TestUtil.java
+++ WeightWatchers_ByBilal/src/weightwatchersassignment/TestUtil.java
@@ -12,10 +12,10 @@ public class TestUtil extends BaseClass {
 	public static long IMPLICIT_WAIT = 10;
 	public static long PAGE_TIMEOUT = 40;
 	
-	public void captureScreenShot(String username) {
+	public void captureScreenShot(String methodname) {
 		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		try {
-			FileUtils.copyFile(src, new File("//Volumes//Biaz -MedD1//bill//biaz//WeightWatchers_ByBilal//resources//"+(username)+".png"));
+			FileUtils.copyFile(src, new File("//Volumes//Biaz -MedD1//bill//biaz//WeightWatchers_ByBilal//resources//"+(methodname)+".png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
